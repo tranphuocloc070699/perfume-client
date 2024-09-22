ab67c261cee4168fa46ce6a20e75e507c02c35c7
tranphuocloc <tranphuocloc070699@gmail.com>
2024-09-22 09:35:09 +0700

    feat: switch computer

--- next.config.mjs
+++ next.config.mjs
@@ -9,6 +9,14 @@ const nextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: "/api/:path*",
+        destination: "http://localhost/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
